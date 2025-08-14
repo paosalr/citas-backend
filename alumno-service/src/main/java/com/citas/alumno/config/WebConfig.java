97b30596cbb58be93350d1fa51ebfbcc6e5a6105
Litzi-Otero <2022371132@uteq.edu.mx>
2025-08-14 15:53:00 -0600

    Conexion con el frontend

---
+++ alumno-service/src/main/java/com/citas/alumno/config/WebConfig.java
@@ -0,0 +1,18 @@
+// src/main/java/com/citas/alumno/config/WebConfig.java
+package com.citas.alumno.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Origen del frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .allowCredentials(true); // Permitir credenciales (si es necesario)
+    }
+}
